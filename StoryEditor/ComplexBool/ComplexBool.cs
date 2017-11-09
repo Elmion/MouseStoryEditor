96c91ffe5b47d5f6d0c67459a73721d65a7c1aa6
Евгений <firegeometry@gmail.com>
2017-11-09 23:27:24 +1000

    1231

---
+++ StoryEditor/ComplexBool/ComplexBool.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoryEditor.ComplexBool
+{
+    public class ComplexBool
+    {
+        public IComplexBoolItem root;
+
+    }
+    class CBTreeOperation : IComplexBoolItem
+    {
+        IComplexBoolItem Right;
+        IComplexBoolItem Left;
+        bool isAnd;
+        public bool Calculation()
+        {
+                if(isAnd)
+                {
+                    return Right.Calculation() && Left.Calculation();
+                }
+                else
+                {
+                    return Right.Calculation() || Left.Calculation();
+                }   
+        }
+    }
+    class CBTreeSimple : IComplexBoolItem
+    {
+        Func<bool> BoolAction;
+        public CBTreeSimple(Func<bool> boolFunc)
+        {
+            BoolAction = boolFunc;
+        }
+        public bool Calculation()
+        {
+            return BoolAction();
+        }
+    }
+    public interface IComplexBoolItem
+    {
+            bool Calculation();
+    }
+
+}
---
+++ StoryEditor/QuestEntity/DiscriptionBlock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestEntity
+{
+    interface IQuestBlock
+    {
+        void     Check();
+        string   CardDiscription { get;}
+        string[] Variants { get; }
+        bool     SetAnswert(byte answer);
+    }
+}
